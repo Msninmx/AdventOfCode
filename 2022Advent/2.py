e8117b75a10507c1b34112f69cd957c578a66df7
Miguel Nguyen <msninmx@gmail.com>
2023-04-05 10:29:56 -0500

    Refactored into separate functions

--- 2022Advent/2.py
+++ 2022Advent/2.py
@@ -1,33 +1,92 @@
-f = open("2input.txt", "r")
+f = open("inputs/2input.txt", "r").read().splitlines()
 
+wins = {
+    "A": "Y",
+    "B": "Z",
+    "C": "X",
+}
+loses = {
+    "A": "Z",
+    "B": "X",
+    "C": "Y",
+}
+ties = {
+    "A": "X",
+    "B": "Y",
+    "C": "Z",
+}
 scores = {
-    "A": 1,
-    "B": 2,
-    "C": 3,
     "X": 1,
     "Y": 2,
     "Z": 3,
 }
-points = 0
-for line in f:
-    choices = line.split()
-    choices[1].strip()
-    op = choices[0]
-    me = choices[1]
-    points += scores[me]
 
-    if op == "A" and me == "X":
-        points += 3
-    elif op == "A" and me == "Y":
-        points += 6
-    elif op == "B" and me == "Y":
-        points += 3
-    elif op == "B" and me == "Z":
-        points += 6
-    elif op == "C" and me == "Z":
-        points += 3
-    elif op == "C" and me == "X":
-        points += 6
 
-f.close()
-print(points)
+def part_one(lines):
+    # Return points based on win and choice
+    points = 0
+    for i in lines:
+        choices = i.split()
+        op = choices[0]
+        me = choices[1]
+        points += scores[me]
+
+        if op == "A" and me == "X":
+            points += 3
+        elif op == "A" and me == "Y":
+            points += 6
+        elif op == "B" and me == "Y":
+            points += 3
+        elif op == "B" and me == "Z":
+            points += 6
+        elif op == "C" and me == "Z":
+            points += 3
+        elif op == "C" and me == "X":
+            points += 6
+
+    return points
+
+
+def part_two(lines):
+    wins = {
+        "A": "Y",
+        "B": "Z",
+        "C": "X",
+    }
+    loses = {
+        "A": "Z",
+        "B": "X",
+        "C": "Y",
+    }
+    ties = {
+        "A": "X",
+        "B": "Y",
+        "C": "Z",
+    }
+    scores = {
+        "X": 1,
+        "Y": 2,
+        "Z": 3,
+    }
+
+    points = 0
+
+    for i in f:
+        choices = i.split()
+        op = choices[0]
+        me = choices[1]
+        if me == "X":
+            points += 0
+            points += scores[loses[op]]
+        elif me == "Y":
+            points += 3
+            points += scores[ties[op]]
+        elif me == "Z":
+            points += 6
+            points += scores[wins[op]]
+
+    return points
+
+
+print(part_one(f))
+print(part_two(f))
