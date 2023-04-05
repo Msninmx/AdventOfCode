dbd0c4481d452c62463f3ffce3e5691914fd2c52
Miguel Nguyen <msninmx@gmail.com>
2023-04-05 10:47:43 -0500

    Refactored into separate functions

--- 2022Advent/4.py
+++ 2022Advent/4.py
@@ -1,29 +1,34 @@
-f = open("4input.txt", "r")
-
-total = 0
-total_1 = 0
-
-for line in f:
-    hours = str(line).strip()
-    hours = hours.split(',')
-    first = hours[0].split('-')
-    second = hours[1].split('-')
-    first = list(map(int, first))
-    second = list(map(int, second))
-
-    print(first, second)
-
-    if (first[0] <= second[0] and first[1] >= second[1]) or (first[0] >= second[0] and first[1] <= second[1]):
-        print(True)
-        total += 1
-
-    first_hours = set(range(first[0], first[1] + 1))
-    second_hours = set(range(second[0], second[1] + 1))
-
-    if len(first_hours & second_hours) != 0:
-        total_1 += 1
+f = open("inputs/4input.txt", "r").read().splitlines()
 
 
-print(total)
-print(total_1)
-f.close()
+def part_one(lines):
+    # Get total of sets that are subsets
+    total = 0
+    for i in lines:
+        hours = i.split(',')
+        first = list(map(int, hours[0].split('-')))
+        second = list(map(int, hours[1].split('-')))
+
+        if (first[0] <= second[0] and first[1] >= second[1]) or (first[0] >= second[0] and first[1] <= second[1]):
+            total += 1
+    return total
+
+
+def part_two(lines):
+    # Get total of sets that have overlaps
+    total = 0
+    for i in lines:
+        hours = i.split(',')
+        first = list(map(int, hours[0].split('-')))
+        second = list(map(int, hours[1].split('-')))
+        first_hours = set(range(first[0], first[1] + 1))
+        second_hours = set(range(second[0], second[1] + 1))
+
+        if len(first_hours & second_hours) != 0:
+            total += 1
+
+    return total
+
+
+print(part_one(f))
+print(part_two(f))
