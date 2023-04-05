bdf2f55211cc3ccb46bf52513caad9d14ba936ea
Miguel Nguyen <msninmx@gmail.com>
2023-04-05 10:20:44 -0500

    Refactored into separate functions

--- 2022Advent/1.py
+++ 2022Advent/1.py
@@ -1,20 +1,33 @@
-f = open("inputs/1input.txt", "r")
+f = open("inputs/1input.txt", "r").read().splitlines()
 
-totals = []
-total = 0
 
-for line in f:
-    if line != "\n":
-        total += int(line)
-    else:
-        totals.append(total)
-        total = 0
-f.close()
+def part_one(lines):
+    # Take the sum of each group of calories, return the highest one
+    totals = []
+    total = 0
+    for i in lines:
+        if i != "":
+            total += int(i)
+        else:
+            totals.append(total)
+            total = 0
+    totals.sort(reverse=True)
+    return totals[0]
 
-totals.sort(reverse=True)
 
-# Part 1: Elf with highest calories
-print(totals[0])
+def part_two(lines):
+    # Take the sum of each group of calories, return sum of highest 3
+    totals = []
+    total = 0
+    for i in lines:
+        if i != "":
+            total += int(i)
+        else:
+            totals.append(total)
+            total = 0
+    totals.sort(reverse=True)
+    return sum(totals[0:3])
 
-# Part 2: Top 3 elves with highest calories
-print(sum(totals[0:3]))
+
+print(part_one(f))
+print(part_two(f))
